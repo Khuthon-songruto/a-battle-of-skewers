1cc58bba05063ecd71ea821ea220542f4425139e
DoJiSeon <djs6620@gmail.com>
2024-09-29 01:09:12 +0900

    vr grip 애니메이션 구현 완료

---
+++ a battle of skewers/Assets/Scripts/AnimateHandOnInput.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
+    public Animator handAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = pinchAnimationAction.action.ReadValue<float>(); // 트리거 버튼 감지 확인 가능함. 오른쪽 검지의 트리거 버튼이다.
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+    }
+}
